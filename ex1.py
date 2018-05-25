d74ded75d8db4b249144181602ba1f0d2782cca7
mrfaulkner <39602250+mrfaulkner@users.noreply.github.com>
2018-05-25 07:37:30 -0500

    Create ex1.py

---
+++ ex1.py
@@ -0,0 +1,11 @@
+'''
+Statement
+Write a program that takes three numbers and prints their sum. Every number is given on a separate line.
+'''
+
+# This program reads two numbers and prints their sum:
+a = int(input())
+b = int(input())
+print(a + b)
+
+# Can you change it so it can read and sum three numbers?
